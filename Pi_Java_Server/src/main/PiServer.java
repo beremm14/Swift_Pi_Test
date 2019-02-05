53a5cd8884059eac246fb232dfc0ed08b70d7e81
Emil Berger <beremm14@htlkaindorf.at>
2019-02-05 21:55:28 +0100

    Add: Server & Client Projects

---
+++ Pi_Java_Server/src/main/PiServer.java
@@ -0,0 +1,9 @@
+package main;
+
+/**
+ *
+ * @author emil
+ */
+public class PiServer {
+    
+}
---
+++ Pi_Swift_Client/Pi_Swift_Client/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  Pi_Swift_Client
+//
+//  Created by Emil Berger on 05.02.19.
+//
+
+import Foundation
+
+print("Hello, World!")
+
